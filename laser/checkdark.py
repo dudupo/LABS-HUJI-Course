2ce0d4353d55fd2359e7daa43f6619387e0f3292
dudupo <dudupo51@gmail.com>
2021-03-15 11:07:14 +0200

    wave plate exp

--- laser/checkdark.py
+++ laser/checkdark.py
@@ -8,11 +8,21 @@ import pandas
 from scipy.optimize import curve_fit
 
 def extract_coef( time, distance ):
-    def f(x, a, b,c, d ):
-        return a*np.cos(b*x+c)**2 + d  
+    def f(x, a, a2, b, b2, c, c2, d ):
+        return a*np.cos(b*x+c)**2 + a2*np.cos(b2*x+c2)**2 + d 
     popt, pcov = curve_fit(f, time, distance)
     _range = np.linspace( np.min(time) , np.max(time), 100 )
     return popt, (_range, f(_range, *popt)) 
+
+
+def extract_coef_onecos( time, distance ):
+    def f(x, a, b, c, d ):
+        return a*np.cos(b*x+c)**2 
+    popt, pcov = curve_fit(f, time, distance)
+    _range = np.linspace( np.min(time) , np.max(time), 100 )
+    return popt, (_range, f(_range, *popt)) 
+
+
 
 """ cm """
 LASER_POSITION = 89 
@@ -38,16 +48,27 @@ ANGELS2 = [ 0 , 10 ,20 ,30, 40 ,50 ,60, 70, 80, 90, 100, 110, 120, 130 ,140, 150
 AMP2 = [ 1.73, 2.03, 2.31, 2.51, 2.50, 2.32, 2.07, 1.74, 1.54, 1.53, 1.65, 1.78, 1.92, 1.944, 1.81, 1.63, 1.52, 1.559, 1.737, 2.03, 2.32, 2.52]
 
 
+"in the dark"
+ANGELS3 = [ 0 , 10 ,20 ,30, 40 ,50 ,60, 70, 80, 90, 100, 110, 120, 130 ,140, 150, 160, 170, 180, 190, 200, 210 ]
+AMP3 = [ 129, 332, 885, 1720, 2700, 3900, 4800, 5100, 4930, 4080, 3000, 1930, 1030, 413, 108, 15, 6, 7.5, 48, 260, 700, 1470]
+
+"reva gal"
+ANGELS4 = [ 0 , 10 ,20 ,30, 40 ,50 ,60, 70, 80, 90, 100, 110, 120, 130 ,140, 150, 160, 170, 180, 190, 200, 210 ]
+AMP4x = [ 7.2, 8.5, 12.9, 8.1, 9.3, 46, 180, 410, 710, 1050, 1200, 1280, 1210, 945, 645, 390, 160, 48, 8, 13, 8.7] #8.4
+AMP4y = [ 8.9, 20, 93, 290, 520, 710, 900, 870, 706, 527, 260, 115, 110, 18, 12, 50, 83, 48, 16, 104, 273]
+
+
 def covert_data_frame( df ):
     return df[0][5:].values.astype(float) 
 
 
-
 def plot_phase_diff(angels,  amp):
     x, y = np.deg2rad((np.array(angels) - CONSTAT_POL_ANGLE) % 360), np.array(amp)
-    coeff, cosfunc = extract_coef(x, y)
     plt.plot(x,y)
-    plt.plot(cosfunc[0], cosfunc[1])
+    
+    for _func in [extract_coef_onecos ]:
+        coeff, cosfunc = _func(x, y)
+        plt.plot(cosfunc[0], cosfunc[1])
     plt.show()
 
 def main():
@@ -65,9 +86,14 @@ def main():
     # plt.savefig( "" )
 
     print(np.average( lightoffset ))
-    plot_phase_diff(ANGELS, np.array(AMP) - np.mean( lightoffset ))
-    plot_phase_diff(ANGELS2, np.array(AMP2) - np.mean( lightoffset ))
+    # plot_phase_diff(ANGELS, np.array(AMP) - np.mean( lightoffset ))
+    # plot_phase_diff(ANGELS2, np.array(AMP2)  - np.mean( lightoffset )) # 
+    # plot_phase_diff(ANGELS3, np.array(AMP3) / np.max(np.array(AMP3) ) ) # 
     
+    print( len(AMP4x ), len(AMP4y))
+
+    plt.scatter( AMP4x , AMP4y )
+    plt.show()
 
 if __name__ == "__main__" :
     main()
